eaf4e2147e7cbcda23218080ab9e8fc0ff310626
Kevin Reuning <kevin.reuning@gmail.com>
2023-12-31 21:42:16 -0500

    writing to subfolder(s)

--- _extensions/reporting-website/template-expand.R
+++ _extensions/reporting-website/template-expand.R
@@ -17,6 +17,7 @@ yml_files <- gsub("qmd$", "yml", template_files)
 for (ii in seq_along(template_files)){
 
     template <- readLines(template_files[ii])
+    base_path <- dirname(yml_files[ii])
     all_vars <- read_yaml(yml_files[ii])
 
     if ("data" %in% names(all_vars)) {
@@ -25,27 +26,29 @@ for (ii in seq_along(template_files)){
     }
 
     items <- unlist(all_vars$items, recursive = FALSE)
-    file_out <- template
+    qmd_out <- template
     for (jj in seq_along(items)){
 
         if ("data" %in% names(items[[jj]])) {
             eval(parse(text = paste0("tmp_df <- ", items[[jj]]$data$subset)))
-            write.csv(tmp_df, items[[jj]]$data$filename, row.names = FALSE)
+            file_out <- file.path(base_path, items[[jj]]$data$filename)
+            write.csv(tmp_df, file_out, row.names = FALSE)
 
-            file_out <- gsub("\\{\\{< template data_filename >\\}\\}",
+            qmd_out <- gsub("\\{\\{< template data_filename >\\}\\}",
                              paste0("\"", items[[jj]]$data$filename, "\""),
-                             file_out)
+                             qmd_out)
 
         }
         vars <- items[[jj]]
         vars$data <- NULL
         for (kk in seq_along(vars)) {
-            file_out <- gsub(paste0("\\{\\{< template ",
+            qmd_out <- gsub(paste0("\\{\\{< template ",
                                     names(vars)[kk], " >\\}\\}"),
-                             vars[[kk]], file_out)
+                             vars[[kk]], qmd_out)
         }
 
-        writeLines(file_out, paste0(names(items)[[jj]], ".qmd"))
+        file_out <- file.path(base_path, paste0(names(items)[[jj]], ".qmd"))
+        writeLines(qmd_out, file_out)
     }
 
 }
